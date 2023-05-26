b90fe1a8a97d126575ef871b59f8fd2560e7292c
Tao Lin <831611+mirguest@users.noreply.github.com>
2023-05-26 19:41:58 +0800

    Add DeepL translator.

---
+++ fakeapi/main.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+#-*- coding: utf-8
+import json
+from aiohttp import web
+
+async def handle(request):
+    print("======================================")
+    print(request)
+    print(request.headers)
+    print(request.method)
+    print(request.url)
+    print(await request.text())
+    print("======================================")
+
+    # response_obj = {'status': 'success'}
+    response_obj = [
+        {"translations": [{"text": 'hello world'}]}
+    ]
+
+    return web.json_response(response_obj)
+
+app = web.Application()
+app.router.add_get('/translator', handle)
+app.router.add_get('/translator/', handle)
+app.router.add_get('/translator/ttranslatev3', handle)
+app.router.add_get('/translator/tlookupv3', handle)
+
+app.router.add_post('/translator', handle)
+app.router.add_post('/translator/', handle)
+app.router.add_post('/translator/ttranslatev3', handle)
+app.router.add_post('/translator/tlookupv3', handle)
+
+app.router.add_route('*', '/{tail:.*}', handle)
+
+web.run_app(app)
